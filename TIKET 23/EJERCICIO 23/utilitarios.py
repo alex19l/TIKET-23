1b6588bb86eac70aabf578933104b6229c7b92aa
IDIC <alumnos@alumnosuio.ute.edu.ec>
2024-12-03 20:01:46 -0500

    Correcion error multa

--- TIKET 23/EJERCICIO 23/utilitarios.py
+++ TIKET 23/EJERCICIO 23/utilitarios.py
@@ -2,3 +2,18 @@ def calcularPromedio(val1,val2,val3):
     calculo=val1+val2+val3
     promedio1=calculo/3
     return promedio1
+
+def consultarMulta(valor): 
+    if valor == 1:
+        return 10 / 100 * 100
+    elif valor == 2:
+        return 15 / 100 * 100
+    elif valor == 3:
+        return 20 / 100 * 100
+    elif valor == 4:
+        return 30 / 100 * 100
+    else:
+        return -1
+     
+        
+    
